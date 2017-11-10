db128b8b6a3e6e2844d2a94c2d6c3641487c3bc3
Aniello Scartozzi <ascartoz@hawk.iit.edu>
2017-11-10 02:35:28 -0600

    added maps with example marker

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,17 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function initMap() {
+    var iit = {lat: 41.8349, lng: -87.6270};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: iit
+    });
+    var marker = new google.maps.Marker({
+      position: iit,
+      map: map
+    });
+  }
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function mobileNav() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
